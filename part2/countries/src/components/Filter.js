88c06a60dba11db449d99f05a954fc98d59ede2e
ireshanwaiman94 <98511645+ireshanwaiman94@users.noreply.github.com>
2023-03-13 23:20:42 +0530

    removed part 2

--- part2/countries/src/components/Filter.js
+++
@@ -1,8 +0,0 @@
-const Filter = ({ value, onChange }) => {
-    return (
-        <div>
-            find countries  <input value={value} onChange={onChange} />
-        </div>
-    )
-}
-export default Filter;
